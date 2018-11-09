746b8872f511eb366bc46fb398d5dbee1defb7b3
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-09 01:44:23 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,15 +2,15 @@
 
 def countdown(n)
   while n >= 0
-    n -= 1
     puts "#{n} SECOND(S)"
+    n -= 1
   end
 end
 
 def countdown_with_sleep(n)
   while n >= 0
-    n -= 1
     puts "#{n} SECOND(S)"
+    n -= 1
     sleep(1)
   end
 end
